7e0c8cb2311069e1619cff24e934829ceae8d283
foisal-hossen <farjana01811389904@gmail.com>
2022-09-04 12:43:49 +0600

    button category name add

--- news.js
+++ news.js
@@ -4,8 +4,21 @@ const loadNews = async () => {
   const url = ` https://openapi.programming-hero.com/api/news/categories`
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.data);
+  displayNews(data.data.news_category);
 }
 
-const displayNews = butto
+const displayNews = button => {
+  const buttonsContainer = document.getElementById('button-container');
+  
+  button.forEach(buttonData =>{
+    
+    const li = document.createElement('li');
+    
+    li.innerHTML = `
+  <button onclick=" "> ${buttonData.category_name} </button>
+  ` ;
+    buttonsContainer.appendChild(li);
+  })
+}
+// load button complete
 loadNews();
